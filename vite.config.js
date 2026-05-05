5a5dde71d871b76b5f3b1f1f9bc582a9721fe968
Arif Yazici <tercihtl.com@gmail.com>
2026-05-05 04:12:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/goit-react-hw-03/',
+  // base: '/goit-react-hw-03/',  <-- Bu satırı siliyoruz veya başına // koyuyoruz
 })
